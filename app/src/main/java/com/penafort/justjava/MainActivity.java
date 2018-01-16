1c852e9c4e505f4c70ea0b9259ee14e8986d3e8d
Luis Penafort <lpenafort88@hotmail.com>
2018-01-16 15:58:00 -0500

    Added code to modify the price order value. Modified code to calculate the quantity with a fixed price. Modified the activity_main.xml to include price label and value

--- app/src/main/java/com/penafort/justjava/MainActivity.java
+++ app/src/main/java/com/penafort/justjava/MainActivity.java
@@ -16,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int quantity=2;
+        display(quantity);
+        displayPrice(quantity*5);
     }
 
     /**
@@ -25,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_value);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView=(TextView)findViewById(R.id.price_value);
+        priceTextView.setText(number);
     }
 }
