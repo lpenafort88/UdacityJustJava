6efa82f21f44a8d2cfa843a4e6de3107b393d741
Luis Penafort <lpenafort88@hotmail.com>
2018-01-18 09:24:36 -0500

    Modified code to have getter of the quantity Text view added methods to increase/decrease quantity and update display Added button views for increasing/decreasing

--- app/src/main/java/com/penafort/justjava/MainActivity.java
+++ app/src/main/java/com/penafort/justjava/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int quantity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity=2;
-        display(quantity);
         displayPrice(quantity*5);
     }
 
@@ -25,12 +23,38 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_value);
-        quantityTextView.setText("" + number);
+        TextView quantityTextView = getQuantityTV();
+        quantityTextView.setText(String.valueOf(number));
     }
-
+    /**
+    *This methods retrieves the view that will display the total price
+     */
     private void displayPrice(int number){
-        TextView priceTextView=(TextView)findViewById(R.id.price_value);
-        priceTextView.setText(number);
+        String price="$"+String.valueOf(number);
+        TextView priceTextView=findViewById(R.id.price_value);
+        priceTextView.setText(price);
+    }
+    /**
+     * This method will increment the counter and update the quantity being displayed
+     */
+    public void increment(View view){
+        quantity=getQuanity();
+        quantity+=1;
+        display(quantity);
+    }
+    /**
+     * This method will decrement the counter and update the quantity being displayed
+     */
+    public void decrement(View view){
+        quantity=getQuanity();
+        quantity-=1;
+        display(quantity);
+    }
+    public TextView getQuantityTV(){
+        return findViewById(R.id.quantity_value);
+    }
+    private int getQuanity(){
+        TextView quantityTV=getQuantityTV();
+        return Integer.parseInt(quantityTV.getText().toString());
     }
 }
